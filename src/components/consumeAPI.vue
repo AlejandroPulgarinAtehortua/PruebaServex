67a60d6e74782c78b0a3ddca7e2d2b90f93d9485
AlejandroPulgarinAtehortua <alejandropulgarinatehortua@gmail.com>
2021-05-26 20:21:05 -0500

    Conf API

---
+++ src/components/consumeAPI.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="API" id="consumeAPI">
+        <h1>Consume la API de Rick and Morty</h1>
+        <p>{{x}}</p>
+        
+    </div>
+</template>
+<script>
+    export default{
+        name:'consumeAPI',
+        datos(){
+            return{
+                consumeAPI: fetch ("https://rickandmortyapi.com/api/character").then((response)=>response.json()),
+                x: console.log(fetch ("https://rickandmortyapi.com/api/character").then((response)=>response.json()))                
+                }
+        }
+        }
+    
+    
+</script>
